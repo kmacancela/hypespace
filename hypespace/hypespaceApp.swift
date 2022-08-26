abc70b43a0acbc6e66f98b2a00f60f8a54a8c9c6
Karina Macancela <karymacancela@gmail.com>
2022-08-25 21:18:50 -0400

    Initial Commit

---
+++ hypespace/hypespaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  hypespaceApp.swift
+//  hypespace
+//
+//  Created by Karina Macancela on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct hypespaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
